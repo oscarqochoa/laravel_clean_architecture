57a1df8febef5b1e590fd3ccdfa386a165f7909f
Alejandro <oscarqochoa@gmail.com>
2021-11-24 19:57:30 -0500

    validate_procedure

--- database/migrations/2021_11_25_003032_sp_gen_example.php
+++ database/migrations/2021_11_25_003032_sp_gen_example.php
@@ -14,7 +14,7 @@ class SpGenExample extends Migration
     public function up()
     {
         $procedure = "
-        
+
         SELECT 
 					cl.id,
 					cl.client_name,
@@ -33,6 +33,38 @@ class SpGenExample extends Migration
 				LEFT JOIN payments pa ON cl.id = pa.client_id
 				WHERE cl.state = 1
 				GROUP BY cl.id
+        ";
+
+        $validate_procedure = "
+        
+        SELECT cl.id,
+       cl.client_name,
+       cl.last_name,
+       cl.dob,
+       cl.phone,
+       cl.email,
+       cl.address,
+       IF(SUM(pa.amount) IS NULL, 0, SUM(pa.amount)) AS amount,
+       COUNT(pa.amount)                              AS total,
+       (
+           SELECT IF( json_arrayagg(
+                          JSON_OBJECT(
+                                  'amount', pa1.amount,
+                                  'transaction_date', pa1.tr_date
+                              )
+                      ) IS NULL, json_array(), json_arrayagg(
+                          JSON_OBJECT(
+                                  'amount', pa1.amount,
+                                  'transaction_date', pa1.tr_date
+                              )
+                      ))
+           FROM payments pa1
+           WHERE pa1.client_id = cl.id
+       )                                                payments_array
+FROM clients cl
+         LEFT JOIN payments pa ON cl.id = pa.client_id
+WHERE cl.state = 1
+GROUP BY cl.id
         ";
     }
 
